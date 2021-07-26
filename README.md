21124c180463e16414821af819d4a1f686e6e576
riju18 <rijuchowdhury18@gmail.com>
2021-07-26 20:07:20 +0600

    update readme & bubble sort

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Data Structure &amp; Algorithm
+
+## Sorting:
++ bubble
++ insertion
++ merge
++ quick
++ selection
+
+## Recursion
+
+## Search:
++ binary
++ linear
+
+## Linear Data Structure:
++ link list
++ stack
++ queue
+
+## Non-Linear Data Structure:
++ tree
--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,8 +1,11 @@
 def bubbleSort(x):
+    count = 0
     for i in range(len(x) - 1, 0, -1):
         for j in range(i):
             if x[j] > x[j + 1]:
+                count += 1
                 x[j], x[j + 1] = x[j + 1], x[j]
+    print(f'no of times swapped: {count}')
 
 
 arr = [84, 21, 96, 15, 47]
