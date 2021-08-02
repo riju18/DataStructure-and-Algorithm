4ffc881045499c6716b61691e1686be83d043883
riju18 <rijuchowdhury18@gmail.com>
2021-08-02 20:53:24 +0600

    value by pos, pos by value fn added & minimized code

--- LinearDataStructure/link_list/1.0singly.py
+++ LinearDataStructure/link_list/1.0singly.py
@@ -13,14 +13,32 @@ class linkedList:       # 2
             self.start = Node(val)
             return
         current = self.start
-        while current.next is not None:
+        while current.next:
             current = current.next
         current.next = Node(val)
 
     def show(self):
         current = self.start
-        while current is not None:
+        while current:
             print(current.value)
+            current = current.next
+
+    def showByPos(self, ind):
+        pos = 0
+        current = self.start
+        while current:
+            if pos == ind:
+                print(current.value)
+            pos += 1
+            current = current.next
+
+    def showPosByVal(self, val):
+        pos = 0
+        current = self.start
+        while current:
+            if current.value == val:
+                print(pos)
+            pos += 1
             current = current.next
 
 
@@ -29,3 +47,5 @@ linkList.insert(2)
 linkList.insert(5)
 linkList.insert(3)
 linkList.show()
+linkList.showByPos(1)
+linkList.showPosByVal(5)
