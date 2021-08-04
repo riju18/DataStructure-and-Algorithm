673cdd3027cbfc8448b0e22fc6d36c748d399d8c
riju18 <rijuchowdhury18@gmail.com>
2021-08-04 12:00:29 +0600

    added descending order fn

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,4 +1,4 @@
-def quick_sort(arr):
+def quick_sort_asc(arr):
     if len(arr) <= 1:
         return arr
 
@@ -13,9 +13,28 @@ def quick_sort(arr):
         else:
             item_lower.append(item)
 
-    return quick_sort(item_lower) + [pivot] + quick_sort(item_greater)
+    return quick_sort_asc(item_lower) + [pivot] + quick_sort_asc(item_greater)
+
+
+def quick_sort_dsc(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr.pop()
+
+    item_greater = []
+    item_lower = []
+
+    for item in arr:
+        if item > pivot:
+            item_greater.append(item)
+        else:
+            item_lower.append(item)
+
+    return quick_sort_dsc(item_greater) + [pivot] + quick_sort_dsc(item_lower)
 
 
 arr_items = [5, 3, 1, 2, 4, 9, 0]
-print(quick_sort(arr_items))
-print(quick_sort(arr_items)[::-1])
+arr_items1 = [5, 3, 1, 2, 4, 9, 0]
+print(quick_sort_asc(arr_items))
+print(quick_sort_dsc(arr_items1))
