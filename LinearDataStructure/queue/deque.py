985b5a010e2d1b7298c080dabaa605534c2df3be
riju18 <rijuchowdhury18@gmail.com>
2022-04-19 19:33:14 +0600

    q vs deque

---
+++ LinearDataStructure/queue/deque.py
@@ -0,0 +1,49 @@
+# *************************************************
+# Principle
+# ============
+# new items can be added/removed either front/rear
+# ************************************************
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def addFront(self, i):  # nq
+        self.items.append(i)
+        print(f"After add front: {self.items}")
+
+    def addRear(self, i):
+        self.items.insert(0, i)
+        print(f"After add rear: {self.items}")
+
+    def removeFront(self):
+        self.items.pop()
+        print(f"After remove front: {self.items}")
+
+    def removeRear(self):   # dq
+        self.items.pop(0)
+        print(f"after remove rear: {self.items}")
+
+    def getItems(self):
+        print(f"All items: {self.items}")
+
+    def getSize(self):
+        print(f"length of items: {len(self.items)}")
+
+
+if __name__ == '__main__':
+    q = Queue()
+    q.addFront(1)
+    q.addFront(2)
+    q.addFront(3)
+
+    q.addRear(4)
+    q.addRear(5)
+    q.addRear(6)
+
+    q.removeFront()
+    q.removeRear()
+
+    q.getItems()
+    q.getSize()
--- LinearDataStructure/queue/q.py
+++ LinearDataStructure/queue/q.py
@@ -1,22 +1,21 @@
+# ************************
+# Principle
+# ============
+# FIFO: First in first out
+# *************************
+
+
 class Queue:
     def __init__(self):
         self.items = []
 
-    def addFront(self, i):
+    def nq(self, i):
         self.items.append(i)
-        print(f"After add front: {self.items}")
+        print(f"After nq: {self.items}")
 
-    def addRear(self, i):
-        self.items.insert(0, i)
-        print(f"After add rear: {self.items}")
-
-    def removeFront(self):
-        self.items.pop()
-        print(f"After remove front: {self.items}")
-
-    def removeRear(self):
+    def dq(self):
         self.items.pop(0)
-        print(f"after remove rear: {self.items}")
+        print(f"after dq: {self.items}")
 
     def getItems(self):
         print(f"All items: {self.items}")
@@ -27,16 +26,12 @@ class Queue:
 
 if __name__ == '__main__':
     q = Queue()
-    q.addFront(1)
-    q.addFront(2)
-    q.addFront(3)
+    q.nq(1)
+    q.nq(2)
+    q.nq(3)
 
-    q.addRear(4)
-    q.addRear(5)
-    q.addRear(6)
-
-    q.removeFront()
-    q.removeRear()
+    q.dq()
+    q.dq()
 
     q.getItems()
     q.getSize()
