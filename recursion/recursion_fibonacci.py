3a62acc4f69d97d7ff4e89310f86850b5bd9fc40
riju18 <rijuchowdhury18@gmail.com>
2022-05-03 11:02:22 +0600

    seq rearrange

--- recursion/recursion_fibonacci.py
+++ recursion/recursion_fibonacci.py
@@ -4,13 +4,15 @@ import time
 
 @lru_cache(maxsize=None)
 def fibo(n):
-    if n == 1 or n == 2:
+    if n == 1:
+        return 0
+    if n == 2:
         return 1
     else:
         return fibo(n - 1) + fibo(n - 2)
 
 
 start = time.time()
-print(fibo(10))
+print(fibo(5))
 end = time.time() - start
 print(f'{end} seconds')
