a6c2510cf16f29dfaff3abc8ea00da459cdf2686
riju18 <rijuchowdhury18@gmail.com>
2022-10-22 10:09:12 +0600

    updated sub-str value

--- Pattern sliding window/longest_substr_length_of_distinct_char.py
+++ Pattern sliding window/longest_substr_length_of_distinct_char.py
@@ -4,22 +4,24 @@
 the_str = "cbbebi"
 k = 3
 hash_map = {}
-win_start = 0
-max_length = 0
-sub_str_arr = []
+win_start, max_length = 0, 0
+sub_str_arr = ''
 for win_end in range(len(the_str)):
     right_char = the_str[win_end]
     if right_char not in hash_map:
         hash_map[right_char] = 0
     hash_map[right_char] += 1
-    while len(hash_map) > k:  # sub_str > k
-        sub_str_arr.append(the_str[win_start: win_end])
+    print(f"hash_map: {hash_map}")
+    while len(hash_map) > k:  # sub_str length > k
+        print(f"length of hash_map: {len(hash_map)}")
         left_char = the_str[win_start]
+        print(f"left char: {left_char}")
         hash_map[left_char] -= 1
         if hash_map[left_char] == 0:
             del hash_map[left_char]
+        print(f"new hash_map: {hash_map}")
         win_start += 1
     max_length = max(max_length, win_end - win_start + 1)
+    sub_str_arr = the_str[win_start: win_end+1]
 print(f"max_length: {max_length}")
-result = list(filter(lambda i: len(i) == max_length, sub_str_arr))
-print(f"longest sub-str: {result}")
+print(f"longest sub-str: {sub_str_arr}")
