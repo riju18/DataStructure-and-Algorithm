c18ad3d3fad52029296e565ed67ec13cab2bd9b1
riju18 <rijuchowdhury18@gmail.com>
2022-10-28 10:27:20 +0600

    longest sub str with k replacement

---
+++ Pattern sliding window/longest_substr_with_same_letters_after_replacement.py
@@ -0,0 +1,20 @@
+# Longest Substring with Same Letters after Replacement
+# =====================================================
+
+the_string = "aabccbb"
+k = 2
+max_repeated_char, win_start, max_length = 0, 0, 0
+final_longest_sub_str = ''
+hash_map = {}
+for win_end in range(len(the_string)):
+    right_char = the_string[win_end]
+    if right_char not in hash_map:
+        hash_map[right_char] = 0
+    hash_map[right_char] += 1
+    max_repeated_char = max(max_repeated_char, hash_map[right_char])
+    if (win_end - win_start + 1 - max_repeated_char) > k:  # to check 2 chars are repeated or not
+        win_start += 1  # shrink the window
+    max_length = max(max_length, win_end-win_start+1)
+    final_longest_sub_str = the_string[win_start:win_end+1]
+print(f"max_str_length: {max_length}")
+print(f"longest_sub_str: {final_longest_sub_str}")
