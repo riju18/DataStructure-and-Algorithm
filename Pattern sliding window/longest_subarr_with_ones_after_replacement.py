b6cf41fc0a07d9afdeaad4b9f1813c71ed7c78e1
riju18 <rijuchowdhury18@gmail.com>
2022-10-29 11:57:09 +0600

    updated code of sub_arr portion in largestSubarrWithOneReplacement

--- Pattern sliding window/longest_subarr_with_ones_after_replacement.py
+++ Pattern sliding window/longest_subarr_with_ones_after_replacement.py
@@ -1,13 +1,20 @@
 the_arr = [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1]
 k = 3
 repeated_one, win_start, max_length = 0, 0, 0
+sub_arr = []
 for win_end in range(len(the_arr)):
     right_char = the_arr[win_end]
+    print(f"i: {win_end}, right_char: {right_char}")
     if right_char == 1:
         repeated_one += 1
+    print(f"repeated_one: {repeated_one}")
     if (win_end-win_start+1-repeated_one) > k:
         if the_arr[win_start] == 1:
             repeated_one -= 1
         win_start += 1
+    if (win_end-win_start+1) > max_length:
+        sub_arr = the_arr[win_start:win_end+1]
     max_length = max(max_length, win_end-win_start+1)
-print(max_length)
+    print(f"max_length: {max_length}")
+print(f"Final max_length: {max_length}")
+print(f"Final sub array: {sub_arr}")
