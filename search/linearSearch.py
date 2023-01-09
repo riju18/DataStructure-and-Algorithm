393143bb0d6ae5c280d38cdeb2194bac60cc86d1
riju18 <rijuchowdhury18@gmail.com>
2023-01-09 22:53:54 +0600

    linear search alternative solution

--- search/linearSearch.py
+++ search/linearSearch.py
@@ -9,6 +9,18 @@ def linearSearch(listItem, key):
     return flag
 
 
+# or,
+
+def linearSearch1(listItem, key):
+    flag = False
+    for i in listItem:
+        if i == key:
+            flag = True
+            break
+
+    return flag
+
+
 demoList = [84, 21, 47, 96, 15]
-result = 'key found..' if linearSearch(demoList, 96) is True else 'key not found..'
+result = 'key found..' if linearSearch1(demoList, 96) is True else 'key not found..'
 print(result)
