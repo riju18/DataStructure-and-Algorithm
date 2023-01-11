a8b9cf747a34ad89f7121ce07b611e1e6a4de7cc
riju18 <rijuchowdhury18@gmail.com>
2023-01-11 20:33:45 +0600

    bubble sort for asc & desc order

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,13 +1,25 @@
-def bubbleSort(x):
+def bubble_sort(x):
     count = 0
     for i in range(len(x) - 1, 0, -1):
         for j in range(i):
             if x[j] > x[j + 1]:
                 count += 1
                 x[j], x[j + 1] = x[j + 1], x[j]
-    print(f'no of times swapped: {count}')
+    print(f'no of times swapped for ascending order: {count}')
+
+
+def bubble_sort_desc(x):
+    count = 0
+    for i in range(len(x) - 1, 0, -1):
+        for j in range(i):
+            if x[j] < x[j + 1]:
+                count += 1
+                x[j + 1], x[j] = x[j], x[j + 1]
+    print(f'no of times swapped for descending order: {count}')
 
 
 arr = [84, 21, 96, 15, 47]
-bubbleSort(arr)
-print(arr)
+bubble_sort(arr)
+print(f"ascending order: {arr}")
+bubble_sort_desc(arr)
+print(f"descending order: {arr}")
